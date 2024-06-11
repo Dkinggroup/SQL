fc23c85248052bb079ca478f682c4ceafad53435
タイン <118415551+thanhchiyeukurumi@users.noreply.github.com>
2024-06-11 13:54:16 +0700

    Update func_da_test.sql

--- func_da_test.sql
+++ func_da_test.sql
@@ -415,6 +415,152 @@ $$ LANGUAGE plpgsql;
 ------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
+😊--Top 10 khách hàng mua từ trước đến hiện tại (chỉ tính tạo acc)
+create or replace function top10_cus()
+returns table(
+	Name varchar(50),
+	score int
+) as $$
+begin 
+	return query
+	select c.name_customer as Name, a.score as score
+	from account a left join customer c on a.phone = c.phone
+	order by a.score desc
+	limit 10;
+end;
+$$ language plpgsql;
+
+select * from top10_cus()
+--cf test ok
+----------------------------------------------------------------------------------------------
+😊--Lich su mua hang cua cus (neu co acc)
+create or replace function history_purchase_cus(in phonee varchar)
+returns table(
+	Date_Order date,
+	Food_Name varchar(100),
+	Quantity int
+) as $$
+begin
+	return query
+	select b.date_order as Date_Order, 
+			f.name_food as Food_Name,
+			bl.quantity as Quantity
+	from customer c
+		right join account a on a.phone = c.phone
+		join bill b on c.id_customer = b.id_customer
+		join bill_line bl on b.id_bill = bl.id_bill
+		join food f on bl.id_food = f.id_food
+	where a.phone = phonee
+	and b.status_bill = 'true'
+	order by b.date_order desc;
+end;
+$$ language plpgsql;
+
+select * from history_purchase_cus('9383081394')
+--cf test ok
+----------------------------------------------------------------------------------------------
+😊--top 10 loại thức ăn bán được từ trước đến nay
+create or replace function top10_food()
+returns table(
+	Food_Name varchar(100),
+	Quantity int
+) as $$
+begin
+	return query
+	select f.name_food as Food_Name,
+			sum(bl.quantity)::int as Quantity
+	from bill_line bl
+	join food f on bl.id_food = f.id_food
+	join bill b on bl.id_bill = b.id_bill
+	where b.status_bill = 'true'
+	group by f.name_food
+	order by sum(bl.quantity) desc
+	limit 10;
+end;
+$$ language plpgsql;
+
+select * from top10_food()
+--cf test ok
+----------------------------------------------------------------------------------------------
+😊--tinh truoc gia tien cho khach hang(ap dung discout khi co tai khoan)
+
+create or replace function pre_price(in bill_id_in int)
+returns table (
+	bill_id int,
+	total_price float
+) as $$
+begin
+	return query
+	select bl.id_bill as bill_id,
+	--chia truong hop gia tien
+		round( case 
+		   		when a.score > 3000 then sum(f.price * bl.quantity)*0.80
+  				when a.score > 1050 then sum(f.price * bl.quantity)*0.85
+				when a.score > 600 then sum(f.price * bl.quantity)*0.90
+				when a.score > 300 then sum(f.price * bl.quantity)*0.95
+		  		else sum(f.price * bl.quantity) 
+		   end 
+		   ) as total_price
+	--ket thuc chia gia tien
+	from bill_line bl
+	join food f on bl.id_food = f.id_food
+	join bill b on bl.id_bill = b.id_bill
+	join customer c on b.id_customer = c.id_customer
+	left join account a on c.phone = a.phone
+	where bl.id_bill = bill_id_in
+	group by bl.id_bill, a.score;
+end;
+$$ language plpgsql;
+
+select * from pre_price(10001)
+--cf test of
+----------------------------------------------------------------------------------------------
+--top 5 mua nhieu nhat cua khach
+create or replace function top5_food_cus(in cus_phone varchar)
+returns table(
+	food_name varchar(100),
+	total_quan bigint
+)	as $$
+begin
+	return query
+	select f.name_food, sum(bl.quantity) astotal_quan
+	from food f
+	join bill_line bl on f.id_food = bl.id_food
+	join bill b on bl.id_bill = b.id_bill
+	join customer c on b.id_customer = c.id_customer
+	where c.phone = cus_phone
+	and b.status_bill = 'true'
+	group by f.name_food
+	order by sum(bl.quantity) desc
+	limit 5;
+end;
+$$ language plpgsql;
+
+
+select * from top5_food_cus('9383081394')
+----------------------------------------------------------------------------------------------
+😊--Update đã thanh toán
+CREATE OR REPLACE FUNCTION cap_nhat_trang_thai_thanh_toan(bill_id INT)
+RETURNS VOID AS $$
+BEGIN
+    UPDATE bill b
+    SET status_bill = 'true'
+    WHERE b.id_bill = bill_id;
+END;
+$$ LANGUAGE plpgsql;
+
+select from cap_nhat_trang_thai_thanh_toan(10001)
+--cf test ok
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
 
 
 
